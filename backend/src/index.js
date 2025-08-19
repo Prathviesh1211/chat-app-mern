94efad2bc42bc04372453b8763080f43d54149e4
Prathviesh1211 <prathvieshnaik@gmail.com>
2025-08-19 16:37:27 +0530

    Added Signup route and tested the endpoint

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,8 +6,11 @@ import dotenv from 'dotenv'
 dotenv.config();
 
 const app=express();
-const PORT=process.env.PORT
-app.use('/',authRoutes)
+const PORT=process.env.PORT;
+
+app.use(express.json());
+app.use('/api/auth/',authRoutes);
+
 
 app.listen(PORT,()=>{
     connectDb();
