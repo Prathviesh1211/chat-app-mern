dba5090f08c1b9196d57a70ca2c128fa08a96229
Prathviesh1211 <prathvieshnaik@gmail.com>
2025-08-20 18:48:09 +0530

    Added login and logout endpoint with proper testing

--- backend/src/controller/auth.controller.js
+++ backend/src/controller/auth.controller.js
@@ -48,9 +48,45 @@ export const signup=async (req,res)=>{
         res.status(500).json({message:"Internal Server Error"})
     }
 }
-export const login=()=>{
-    res.send("Login Page")
+export const login=async (req,res)=>{
+    const {email,password}=req.body;
+    try{
+        if(!email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        if(password.length<6){
+            return res.status(400).json({message:"Password must be atleast 6 characters"});
+        }
+
+        const user=await User.findOne({email});
+        if(!user){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+
+        const isPasswordCorrect=await bcrypt.compare(password,user.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message:"Invalid Credentials"});
+        }
+
+        generateToken(user._id,res);
+        
+        res.status(201).json({
+                _id:user._id,
+                email:user.email,
+                fullName:user.fullName,
+                profilePic:user.profilePic
+            })
+    }catch(error){
+console.error("Error in login controller :",error.message);
+        res.status(500).json({message:"Internal Server Error"})
+    }
 }
-export const logout=()=>{
-    res.send("Logout Page")
+export const logout=async(req,res)=>{
+    try{
+        res.cookie("jwt","",{maxAge:0});
+        res.status(200).json({message:"Logged out succcessfully"})
+    }catch(error){
+        console.error("Error in logout controller :",error.message);
+        res.status(500).json({message:"Internal Server Error"})
+    }
 }
